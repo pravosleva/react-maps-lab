b5166764a9d510353519a53f2626fa543c7657d2
pravosleva <selection4test@gmail.com>
2018-05-25 00:23:49 +0300

    Updates
    - styles updated
    - sweetalert2 added to project

--- src/components/MainWrapper.js
+++ src/components/MainWrapper.js
@@ -5,14 +5,22 @@ import styled from 'styled-components';
 export const MainFlexWrapper = styled('div')`
   font-family: Montserrat;
   display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  align-items: center;
+  flex-flow: row nowrap;
+    /*align-items: center;*/
+  justify-content: flex-start;
+  align-items: stretch;
+  align-content: center;
+
   width: 100%;
 `;
 
 export const MainFlexElement = styled('div')`
-  width: 100%;
-    /*max-width: 600px;*/
-  justify-content: center;
+  &:first-child {
+    padding: 20px;
+    width: 250px;
+    max-width: 250px;
+  }
+  &:nth-child(2n) {
+    width: calc(100% - 250px);
+  }
 `;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom';
 import { Home } from '../components/Home';
 import NotFound from '../components/NotFound';
 import { Example1 } from '../components/Example1';
@@ -8,17 +8,98 @@ import { Example2 } from '../components/Example2';
 
 import { MainFlexWrapper, MainFlexElement } from '../components/MainWrapper';
 
+// Read more about auth needs:
+// https://github.com/ReactTraining/react-router/issues/5155
+
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+
+const routes = [
+  {
+    path: '/',
+    exact: true,
+    main: () => <Home />,
+    link: { text: 'Home', descr: 'This is examples of Google Map usage.' },
+  },
+  {
+    path: '/example1',
+    exact: true,
+    main: () => <Example1 />,
+    link: { text: 'Example1' },
+  },
+  {
+    path: '/example2',
+    exact: true,
+    main: () => <Example2 />,
+    link: { text: 'Example2' },
+  },
+];
 
 const Routes = () => (
 <BrowserRouter>
   <MainFlexWrapper>
     <MainFlexElement>
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
+        {
+          routes.map((route, index) => {
+            if (route.link) {
+              return (
+                <li>
+                  <Link to={route.path}>{route.link.text}</Link>
+                  {route.link.descr ? <p style={{ paddingLeft: '15px' }}>{route.link.descr}</p> : null}
+                </li>
+              )
+            }
+          })
+        }
+        <li>
+          <a href='#' onClick={
+            (e) => {
+              e.preventDefault();
+              Swal({
+                title: 'Are you sure?',
+                text: 'You will be redirected to original article.',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, I got it!',
+                cancelButtonText: 'No'
+              }).then((result) => {
+                if (result.value) {
+                  // go head...
+                } else if (result.dismiss === Swal.DismissReason.cancel) {
+                  Swal(
+                    'Cancelled',
+                    'You will not be redirected.',
+                    'error'
+                  );
+                }
+                return result;
+              }).then((result) => {
+                if (result.value) {
+                  window.location = 'https://tomchentw.github.io/react-google-maps/#usage--configuration';
+                } else {
+                  return false;
+                }
+              });
+            }
+          }
+          >Original doc</a>
+        </li>
+      </ul>
+    </MainFlexElement>
+    <MainFlexElement>
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/example1' component={Example1} />
-        <Route exact path='/example2' component={Example2} />
-        {/*<Route exact path='/example3' component={Example3} />*/}
-
+        {
+          routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.main}
+            />
+          ))
+        }
         <Route exact path='/*' component={NotFound} />
       </Switch>
     </MainFlexElement>
