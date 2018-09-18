6a6663950058876e08701260ca793b733a699dfe
Den Pol <selection4test@gmail.com>
2018-09-18 22:00:46 +0300

    Example12

--- src/components/Examples/Example12/index.js
+++ src/components/Examples/Example12/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Map from 'pigeon-maps';
 import Marker from 'pigeon-marker';
 // import Overlay from 'pigeon-overlay';
+import Cluster from 'pigeon-cluster';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { specialLog } from '../specialLog'; // specialLog('look', null, ['tst']);
@@ -26,25 +27,36 @@ const mapState = ({ markers }) => ({
   items: markers.items,
 });
 
+// const coordinates = [
+//   [lat,lng],
+//   [lat,lng],
+//   [lat,lng]
+// ];
+
 export const Example12 = connect(mapState)(({ items }) => (
   <WrapperContainer>
     <WrapperElement>
       <Map
         center={[-34.8043978515038, 152.37730272101211]}
-        zoom={5}
+        zoom={3}
         // width={600}
         // height={500}
       >
-        {
+        {/*
           items.map((e, i, a) => (
             <Marker key={Math.random()} anchor={[e.lat, e.lng]} payload={i} onClick={({ event, anchor, payload }) => specialLog('event, anchor, payload', null, [event, anchor, payload])} />
           ))
-        }
+        */}
         {/*
         <Overlay anchor={[50.879, 4.6997]} offset={[120, 79]}>
           <img src='pigeon.jpg' width={240} height={158} alt='' />
         </Overlay>
         */}
+        <Cluster>
+          {
+            items.map((e) => [e.lat, e.lng]).map(coordinate => <Marker key={coordinate.toString()} anchor={coordinate} payload={1}/>)
+          }
+        </Cluster>
       </Map>
     </WrapperElement>
   </WrapperContainer>
